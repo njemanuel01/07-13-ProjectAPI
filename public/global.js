4a053e16d1412d020e233f7e2fbbfa8eceedfd73
Nicholas Emanuel <njemanuel01@gmail.com>
2015-07-13 16:47:44 -0500

    Fixed js

--- public/global.js
+++ public/global.js
@@ -1,12 +1,15 @@
 var all_projects = function() {
   var req = new XMLHttpRequest();
+  var names = [];
 
   req.open("get", "/projects");
 
   req.addEventListener("load", function() {
     for (var i = 0; i < req.response.length; i++) {
-      document.getElementById("content").innerHTML = "Name: " + req.response[i].name;
+      names[i] = "Name: " + req.response[i].name; + ", ";
     }
+    
+    document.getElementById("content").innerHTML = names.toString();
   })
 
   req.responseType = "json";
@@ -17,11 +20,20 @@ var all_projects = function() {
 var single_project = function() {
   var req = new XMLHttpRequest();
   var id = document.getElementById("id").value.charAt(0);
+  var links = [];
+  var members = [];
   
   req.open("get", "/project/" + id);
 
   req.addEventListener("load", function() {
-    document.getElementById("content").innerHTML = "Name: " + req.response[0].name;
+    for (var i = 0; i < req.response[1].links.length; i++) {
+      links[i] = req.response[1].links[i].link + ", ";
+    }
+    for (var i = 0; i < req.response[2].members.length; i++) {
+      members[i] = req.response[2].members[i].member + ", ";
+    }
+    var text = "Name: " + req.response[0].name + ", Description: " + req.response[0].description + ", Links: " + links.toString() + ", Members:" + members.toString();
+    document.getElementById("content").innerHTML = text;
   })
 
   req.responseType = "json";
